6a58acdc53708f2d489d397e65b2e239141af9d8
Yağmur K. <yagmurrkeserr1@gmail.com>
2025-04-14 01:49:33 +0300

    Part 2

---
+++ UdemyMyPortfolio/Controllers/ExperienceController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using UdemyMyPortfolio.DAL.Context;
+
+namespace UdemyMyPortfolio.Controllers
+{
+    public class ExperienceController : Controller
+    {
+        UdemyMyPortfolioContext context = new UdemyMyPortfolioContext();
+        public IActionResult ExperienceList()
+        {
+            var values = context.Experiences.ToList();
+            return View(values);
+        }
+    }
+}
---
+++ UdemyMyPortfolio/ViewComponents/LayoutViewComponents/_LayoutSidebarComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace UdemyMyPortfolio.ViewComponents.LayoutViewComponents
+{
+    public class _LayoutSidebarComponentPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
